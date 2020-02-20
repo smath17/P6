36f62b27e3d48f71be2ee26dc265955209ea9ccb
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2020-02-20 11:54:52 +0100

    Initial python UDP

---
+++ UDP_trial/main.py
@@ -0,0 +1,23 @@
+import socket
+
+if __name__ == "__main__":
+
+    msgFromClient = "(init Bobbers)"
+
+    bytesToSend = str.encode(msgFromClient)
+
+    serverAddressPort = ("127.0.0.1", 6000)
+
+    bufferSize = 1024
+
+    # Create a UDP socket at client side
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    # Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+    msg = "Message from Server {}".format(msgFromServer[0])
+
+    print(msg)
