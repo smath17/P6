15ef3bca7bc5828ca3002daf3d16ed921cb34d08
Cuz <37619846+Rynke@users.noreply.github.com>
2020-02-24 13:53:37 +0100

    Up down left right controls

--- source/main.py
+++ source/main.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     bytesToInit = str.encode(initPlayer)
     bytesToMove = str.encode(movePlayer)
     byteDash = str.encode("(dash 100)")
-    byteDashUp = str.encode("(dash 100 90)")
+    byteDashDown = str.encode("(dash 100 90)")
+    byteDashUp = str.encode("(dash 100 -90)")
+    byteDashBack = str.encode("(dash 100 180)")
     byteKick = str.encode("(kick 100 0)")
 
     serverAddressPort = ("127.0.0.1", 6000)
@@ -41,69 +43,26 @@ if __name__ == "__main__":
     UDPClient10Socket.sendto(bytesToInit, serverAddressPort)
     UDPClient11Socket.sendto(bytesToInit, serverAddressPort)
 
-    #UDPClient1Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient2Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient3Socket.sendto(bytesToMove, serverAddressPort)
-    UDPClient4Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient5Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient6Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient7Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient8Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient9Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient10Socket.sendto(bytesToMove, serverAddressPort)
-    #UDPClient11Socket.sendto(bytesToMove, serverAddressPort)
-
-    i = 0
     while True:
-        #i = i + 1
-        #UDPClient4Socket.sendto(bytesToMove, serverAddressPort)
-        #if i % 2 == 0:
-
-        #else:
 
         try:  # used try so that if user pressed other than the given key error will not be shown
-            if keyboard.is_pressed('d'):  # if key 'q' is pressed
+            if keyboard.is_pressed('RIGHT_KEY'):  # if key 'q' is pressed
                 UDPClient4Socket.sendto(byteDash, serverAddressPort)
                 continue  # finishing the loop
-            if keyboard.is_pressed('w'):  # if key 'q' is pressed
+            if keyboard.is_pressed('DOWN_KEY'):  # if key 'q' is pressed
+                UDPClient4Socket.sendto(byteDashDown, serverAddressPort)
+                continue  # finishing the loop
+            if keyboard.is_pressed('UP_KEY'):  # if key 'q' is pressed
                 UDPClient4Socket.sendto(byteDashUp, serverAddressPort)
+                continue  # finishing the loop
+            if keyboard.is_pressed('LEFT_KEY'):  # if key 'q' is pressed
+                UDPClient4Socket.sendto(byteDashBack, serverAddressPort)
                 continue  # finishing the loop
             if keyboard.is_pressed('x'):  # if key 'q' is pressed
                 UDPClient4Socket.sendto(byteKick, serverAddressPort)
                 continue  # finishing the loop
         except:
             continue  # if user pressed a key other than the given key the loop will break
-
-
-        #UDPClient1Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient1Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient2Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient2Socket.sendto(byteKick, serverAddressPort)
-
-        #DPClient3Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient3Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient5Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient5Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient6Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient6Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient7Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient7Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient8Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient8Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient9Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient9Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient10Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient10Socket.sendto(byteKick, serverAddressPort)
-
-        #UDPClient11Socket.sendto(byteDash, serverAddressPort)
-        #UDPClient11Socket.sendto(byteKick, serverAddressPort)
 
         msgFromServer = UDPClient4Socket.recvfrom(bufferSize)
 
