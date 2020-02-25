cb272ab1608d855d274699dc4ef6c9d4f9db990d
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2020-02-25 10:43:21 +0100

    Use class to init team

--- source/PlayerController.py
+++ source/PlayerController.py
@@ -1,20 +1,19 @@
 import socket
 
-class player:
+
+class Player:
 
     def __init__(self, teamname):
-
         # local hosted server on port 6000 by default
         self.serverAddressPort = ("127.0.0.1", 6000)
         # Server messages has to be sent as bytes
-        self.initBytes = str.encode("(init" + teamname + ")")
-
+        initString = "(init" + teamname + ")"
 
         # Create client via UDP socket
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         # Initialize player on team
-        self.UDPClientSocket.sendto(self.initBytes, self.serverAddressPort)
+        self.send_action(initString)
 
     # Wrapper function for UDP communication + byte encoding
     def send_action(self, action):
