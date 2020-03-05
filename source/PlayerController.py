34961ecf8aaea7dd1956da138de1c22555c8ad21
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2020-03-05 13:48:16 +0100

    v16, parser, goalie
    Init goalie
    use version 16
    parse sense, see, play_mode

--- source/PlayerController.py
+++ source/PlayerController.py
@@ -1,3 +1,4 @@
+import atexit
 import socket
 
 
@@ -7,68 +8,45 @@ class Player:
     def __init__(self, teamname, goalie=False):
         self.stamina = 0
         self.speed = 0
+        self.effort = 0
         self.head_angle = 0
         self.tackled = 0
+        self.game_status = ""
 
         # local hosted server on port 6000 by default
         # TODO. IP of the day
-        self.serverAddressPort = ("172.31.253.241", 6000)
+        self.init_port = 6000
+        self.serverAddressPort = ("172.31.253.241", self.init_port)
 
         # Create client via UDP socket
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         # Initialize player on team
         if not goalie:
-            self.initString = "(init " + teamname + ")"
+            self.initString = "(init " + teamname + " (version 16)" + ")"
         else:
-            self.initString = "(init " + teamname + " (goalie))"
+            self.initString = "(init " + teamname + " (version 16) (goalie))"
 
-        self.send_action(self.initString)
+        self.send_init(self.initString)
 
     # Wrapper function for UDP communication + byte encoding
+    def send_init(self, action):
+        self.send_action(action)
+        # Receive server init output and grab dedicated port
+        new_port = self.UDPClientSocket.recvfrom(6000)[1]
+        self.serverAddressPort = ("172.31.253.241", new_port[1])
+
     def send_action(self, action):
-        self.UDPClientSocket.sendto(str.encode(action), self.serverAddressPort)
+        # action is null terminated because server is written in c++
+        self.UDPClientSocket.sendto(str.encode(action + '\0'), self.serverAddressPort)
 
     def rec_msg(self):
         # Receive message from server, decode from bytes
-        msg_from_server = self.UDPClientSocket.recvfrom(1024)
+        msg_from_server = self.UDPClientSocket.recvfrom(6000)
+
         # the received msg is (bytes, encoding) we just want the bytes, hence [0]
+        # print(msg_from_server[0].decode())
         return msg_from_server[0].decode()
 
-        # Works for printing direct message
-        # msg = "Message from server: {}".format(msg_from_server[0].decode())
-        # print(msg)
-
-    # str arg used for testing
-    # TODO: can only be used after kick_off
-    def parse_info(self, rec_msg):
-        # Get the first 5 chars, used to recognize the type of msg
-        msg_type = rec_msg[:5]
-
-        if msg_type == "(sens":
-            # In order to only get the required numbers, we spilt the string by spaces and load it into a list
-            info_list = rec_msg.rsplit(" ")
-            # the 6th element is current stamina
-            self.stamina = int(info_list[6])
-            self.speed = int(info_list[9])
-
-            # TODO: If real, then load into player, if not remove
-            # figure out if effort and directionOfSpeed is real
-            print("Stamina: {}".format(self.stamina))
-            print("Effort: {}".format(info_list[7]))
-            print("direction of speed: {}".format(info_list[10]))
-
-        elif msg_type == "(hear":
-            # referee change mode OR player say
-            pass
-
-        elif msg_type == "(init":
-            # player initialized
-            print("Player connected")
-
-        elif msg_type == "(see ":
-            # Split at (( to get every object and its info separated
-            info_list = rec_msg.rsplit("((")
-            # l = left, r = right, c = center
-            # ((name) distance direction)
-            pass
+    def disconnect(self):
+        self.send_action("(bye)")
--- source/main.py
+++ source/main.py
@@ -1,12 +1,18 @@
 from PlayerController import Player
+from ServerParser import Parser
+
 
 if __name__ == "__main__":
 
-    teamname1 = "Bobzors"
+    teamname1 = "Simon"
     # Create a list of players
     team1 = [Player(teamname1, True)]
     for x in range(10):
         team1.append(Player(teamname1))
+
+    parser = Parser()
+
+
 
     # Initially move all players from team1 onto the field
     y = -30
@@ -16,7 +22,9 @@ if __name__ == "__main__":
         y = y + 5
 
     while True:
-        for player in team1:
-            player.send_action("(dash 100)")
+        #    for player in team1:
+        #        player.send_action("(dash 100)")
 
-        #team1[4].parse_info(team1[4].rec_msg())
+        #team1[0].rec_msg()
+        parser.parse_info(team1[0].rec_msg(), team1[0])
+    # team1[4].parse_info(team1[4].rec_msg())
