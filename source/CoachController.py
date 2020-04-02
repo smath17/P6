8b11217748b3e8a77a4e68c8c190579ec4d932bf
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2020-04-02 12:18:29 +0200

    multiprocess + train session
    Teamsetup creates new process for each player
    Offline coach can now be used for automatic training session

--- source/CoachController.py
+++ source/CoachController.py
@@ -9,9 +9,11 @@ class Coach:
         if offline:
             # Server needs to run with -server::coach=on, to disable the referee
             self.coach_port = 6001
+            self.init_string = "(init (version 16))"
             print("Requires -server::coach=on")
         else:
             self.coach_port = 6002
+            self.init_string = "(init " + teamname + " (version 16))"
 
         # TODO. IP of the day
         self.serverAddressPort = (self.ip, self.coach_port)
@@ -19,24 +21,36 @@ class Coach:
         # Create client via UDP socket
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-        self.init_string = "(init " + teamname + " (version 16))"
+
         self.send_init(self.init_string)
 
     def send_init(self, action):
-        self.send_action(action)
+        # Cannot use send_action, as we need to receive port
+        self.UDPClientSocket.sendto(str.encode(action + '\0'), self.serverAddressPort)
         # Receive server init output and grab dedicated port
         init_msg = self.UDPClientSocket.recvfrom(6000)
         # new port = (ip, port)
         new_port = init_msg[1]
         self.serverAddressPort = (self.ip, new_port[1])
 
+        # Receive 20 inits, these have to be read
+        self.rec_dummy_msg()
+
+    def rec_dummy_msg(self):
+        self.send_action('(look)')
+        for x in range(20):
+            self.rec_msg()
+
     def send_action(self, action):
         # action is null terminated because server is written in c++
         self.UDPClientSocket.sendto(str.encode(action + '\0'), self.serverAddressPort)
+        # return message will have to be received
+        return self.rec_msg()
 
     def rec_msg(self):
         # Receive message from server, decode from bytes
         msg_from_server = self.UDPClientSocket.recvfrom(6000)
+        print(msg_from_server[0].decode())
 
         # the received msg is (bytes, encoding) we just want the bytes, hence [0]
         return msg_from_server[0].decode()
@@ -48,17 +62,17 @@ class Coach:
         self.UDPClientSocket.close()
 
     # Wrapper for move_object_inner, as it needs try/except
-    def move_player(self, player, x, y):
+    def move_player(self, player, teamname, x, y):
         try:
-            self.__move_player_func(player, x, y)
+            self.__move_player_func( player, teamname, x, y)
         except ValueError as err:
             print(err.args)
 
     # Moves the target playerNum on coach's team to x,y coord (PRIVATE)
-    def __move_player_func(self, player, x, y):
+    def __move_player_func(self, player, teamname, x, y):
         if self.coach_port != 6001:
             raise ValueError('Wrong type of coach, should be offline/trainer')
-        self.send_action('(move (p "{}" {}) {} {})'.format(self.teamname, player.unum, x, y))
+        self.send_action('(move (player {} {}) {} {})'.format(teamname, player.unum, x, y))
 
     def move_ball(self, x, y):
         try:
@@ -70,7 +84,38 @@ class Coach:
     def __move_ball_func(self, x, y):
         if self.coach_port != 6001:
             raise ValueError('Wrong type of coach, should be offline/trainer')
-        self.send_action('(move (b) {} {})'.format(x, y))
+        self.send_action('(move (ball) {} {})'.format(x, y))
 
     def reset_ball(self):
         self.move_ball(0, 0)
+
+    def kickoff(self):
+        self.send_action('(start)')
+
+    # Reset stamina
+    def recover(self):
+        self.send_action('(recover)')
+
+    # Returns server answer
+    def check_ball(self):
+        msg = self.send_action('(check_ball)')
+        #msg = self.rec_msg()
+        msg = msg.rsplit(' ')
+        # Ball posistion (goal_r, goal_l, in_field, out_of_field)
+        bPOS = msg[3][:-2]
+        print(bPOS)
+        return bPOS
+
+
+    def goal_basic_training(self, teamname, player):
+        # Initial setup
+        self.move_player(player, teamname, 20, 0)
+        self.kickoff()
+        self.move_ball(30, 0)
+
+        # TODO: should be episode based instead of true
+        while True:
+            if self.check_ball() == "goal_r":
+                self.move_ball(30, 0)
+                self.move_player(player, teamname, 20, 0)
+                # Add points
--- source/main.py
+++ source/main.py
@@ -1,18 +1,15 @@
 from PlayerController import Player
 from CoachController import Coach
 from KeyboardControls import KeyboardControl
+from TeamSetup import TeamSetup
 import sys
 
-if __name__ == "__main__":
 
-    teamname1 = "Simon"
-
+def createTeam():
     # Create a list of players
     team1 = [Player(teamname1, True)]
     for x in range(10):
         team1.append(Player(teamname1))
-
-
 
     # Initially move all players from team1 onto the field
     y = -30
@@ -20,20 +17,33 @@ if __name__ == "__main__":
     for player in team1:
         player.send_action("(move -20 {})".format(y))  # This is a string formatted to include y in the {}
         y = y + 5
+    return team1
+
+
+if __name__ == "__main__":
+
+    teamname1 = "Simon"
 
     # Check for args, argv[0] is the script
     # TODO: improve parameters (multiple)
     if len(sys.argv) > 1:
         if sys.argv[1] == "-trainer":
+            team1 = createTeam()
             coach = Coach(teamname1, True)
         elif sys.argv[1] == "-coach":
+            team1 = createTeam()
             coach = Coach(teamname1)
         elif sys.argv[1] == "-k":
+            team1 = createTeam()
             controller = KeyboardControl()
             controller.keymap(team1)
         elif sys.argv[1] == "-simplescore":
+            team1 = createTeam()
             controller = KeyboardControl()
             controller.simple_auto_score(team1)
-
-    while True:
-        team1[0].update_info()
+        elif sys.argv[1] == "-multiprocess":
+            team = TeamSetup(teamname1)
+        elif sys.argv[1] == "-basetrain":
+            player = Player(teamname1)
+            coach = Coach(teamname1, True)
+            coach.goal_basic_training(player, teamname1)
