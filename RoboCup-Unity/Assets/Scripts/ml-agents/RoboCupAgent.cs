a05d8b6fdda763e28ca6624c3c506386c81ae324
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2020-04-24 13:12:47 +0200

    Added ml-agents package to project
    (Has to be manually downloaded and added to project)

---
+++ RoboCup-Unity/Assets/Scripts/ml-agents/RoboCupAgent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+public class RoboCupAgent : Agent
+{
+    public override void OnEpisodeBegin()
+    {
+    }
+    
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+    
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        //SetReward(1.0f);
+        //EndEpisode();
+    }
+    
+    public override float[] Heuristic()
+    {
+        var action = new float[2];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        return action;
+    }
+}
